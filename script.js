427da83f6d2346e14d5ec3d911681bc7edd23e29
Anthony Nwambu <aonwambu@gmail.com>
2025-06-15 14:52:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
+const { useSyncExternalStore } = require("react");
 
+document.addEventListener = ( 'DOMContentLoaded', function() {
+const form = 
+document.getElementById(id="registration-form");
+
+const feedbackDiv = document.getElementById("id-form-feedback")
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+
+    const username = document.getElementById('username').value.trim();
+
+    let isValid = true;
+    const messages = [];
+
+    if (username.lenght < 3){
+        isValid = false;
+        messages.push("Username must not be less than 3 characters.");
+    }
+
+
+    const email = document.getElementById('email').value.trim();
+    
+    let isValid = true;
+    const messages = [];
+
+    if (!email incudes ('@') || !email includes ('.')){
+        isValid = false;
+        messages.push("Please enter a valid email address");
+    }
+
+
+    const passwordInput = document.getElementById('passwordInput').value.trim();
+
+    let isValid = true;
+    const messages = []
+
+    if (passwordInput length < 8);{
+    isValid = false;
+    messages.push("Enetr a valid password")
+    }
+
+
+    feedbackDiv.style.display = 'Block';
+    if (isValid) = {
+        feedbackDiv.textContent = 'Registration Successful!';
+        feedbackDiv.style.color = '#28a745';
+    } else {
+        feedbackDiv.textContent = 'Please fix all errors before submitting';
+        feedbackDiv.style.color = '#dc3545';
+    }
+      });
+});
