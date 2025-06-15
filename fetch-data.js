0aef241da488b9e571f2a8644d735238af62ba04
Anthony Nwambu <aonwambu@gmail.com>
2025-06-15 21:08:23 +0100

    Update fetch-data.js

--- fetch-data.js
+++ fetch-data.js
@@ -1,1 +1,26 @@
+async function  fetchuserData(){
+    const apiurl = 'https://jsonplaceholder.typicode.com/users';
 
+    const dataContainer = document.getElementById(api-data);
+    try{
+        const response = await fetch (apiUrl);
+        const user = awat response.json();
+
+        dataContainer.innerHTML = '';
+
+        const userList = document.createElement('ul');
+
+      users forEach = (user => {
+        const listItem = document.createElement('li');
+        listItem.textContent = user.name;
+        user.list.appendChild(listItem);
+      });a
+
+      dataContainer = appendChild(userList);
+
+} catch (error){
+    dataContainer.innerHTML = '';
+    dataContainer.textContent = 'Failed to load user data.';
+    }
+}
+document.addEventListener('DOMContentLoaded', fetchUserData);
